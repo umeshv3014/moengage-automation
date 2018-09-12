e4932486ce2c510bdff34f88395e2bd667c6ca46
umesh v <umesh.v@via.com>
2018-09-12 21:39:48 +0530

    removed unwanted data

--- src/main/java/com/moenagage/app/pages/actions/HomePageActions.java
+++ src/main/java/com/moenagage/app/pages/actions/HomePageActions.java
@@ -11,4 +11,5 @@ public class HomePageActions extends HomePage {
 		super(driver);
 	}
 
+	
 }
---
+++ src/main/java/com/moengage/utill/TestCaseExcelConstant.java
@@ -0,0 +1,92 @@
+package com.moengage.utill;
+
+public class TestCaseExcelConstant {
+  public static final int COL_SCRIPT_RUN = 0;
+  public static final int COL_TEST_CASE_ID = 1;
+  public static final int COL_LOGIN = 2;
+  public static final int COL_PROMO_CODE = 3;
+  public static final int COL_PAYMENT_MODE = 4;
+  public static final int COL_PAYMENT_TYPE = 5;
+
+  // Flights
+  public static final int COL_SOURCE = 6;
+  public static final int COL_DESTINATION = 7;
+  public static final int COL_SOURCE1 = 8;
+  public static final int COL_DESTINATION1 = 9;
+  public static final int COL_ONWARD_DATE_ADD = 10;
+  public static final int COL_RETURN_DATE_ADD = 11;
+  public static final int COL_ADULT = 12;
+  public static final int COL_CHILD = 13;
+  public static final int COL_INFANT = 14;
+  public static final int COL_STOP = 15;
+  public static final int COL_CLASS = 16;
+  public static final int COL_ONWARD_FLIGHT = 17;
+  public static final int COL_RETURN_FLIGHT = 18;
+  public static final int COL_ONWARD_SSR = 19;
+  public static final int COL_RETURN_SSR = 20;
+
+  // Hotels
+  public static final int COL_HOTEL_CITY = 6;
+  public static final int COL_HOTEL_NAME = 7;
+  public static final int COL_HOTEL_CHECKIN = 8;
+  public static final int COL_HOTEL_CHECKOUT = 9;
+  public static final int COL_HOTEL_ROOMS = 10;
+  public static final int COL_HOTEL_GUESTS = 11;
+  public static final int COL_HOTEL_AGE = 12;
+  public static final int COL_HOTEL_FLOW = 13;
+
+  // Holidays
+
+  public static final int COL_HOLIDAY_BOOKINGTYPE = 6;
+  public static final int COL_HOLIDAY_DESTINATION = 7;
+  public static final int COL_HOLIDAY_DESTINATION_TYPE = 8;
+  public static final int COL_HOLIDAY_CHECKINDATE = 9;
+  public static final int COL_HOLIDAY_ROOMCOUNT = 10;
+  public static final int COL_HOLIDAY_GUESTCOUNT = 11;
+  public static final int COL_HOLIDAY_CHILDRENCOUNT = 12;
+  public static final int COL_HOLIDAY_BEDREQUIREMENT = 13;
+
+  // Topup
+  public static final int COL_TOPUP_TYPE = 6;
+  public static final int COL_TOPUP_OPERATOR_CIRCLE = 7;
+  public static final int COL_TOPUP_SERVICE_OPERATOR = 8;
+  public static final int COL_TOPUP_SUB_SERVICE_OPERATOR = 9;
+  public static final int COL_TOPUP_SUB_SUB_SERVICE_OPERATOR = 10;
+  public static final int COL_TOPUP_AMOUNT = 11;
+  public static final int COL_TOPUP_MOBILE_NUMBER = 12;
+  public static final int COL_TOPUP_ACCOUNT_NUMBER = 13;
+
+  // Money Transfer
+  public static final int COL_MONEY_TRANSFER_SENDER_MOBILE_NO = 6;
+
+  // Buses
+  public static final int COL_BUS_SOURCE = 6;
+  public static final int COL_BUS_DESTINATION = 7;
+  public static final int COL_BUS_ONWARD_DATE = 8;
+  public static final int COL_BUS_RETURN_DATE = 9;
+  public static final int COL_BUS_SEATS = 10;
+  public static final int COL_BUS_ONWARD_OPERATOR = 11;
+  public static final int COL_BUS_RETURN_OPERATOR = 12;
+
+  // giftCard
+  public static final int COL_GIFTCARD_PRODUCTNAME = 6;
+
+  // mice
+  public static final int COL_MICE_JOURNEYTYPE = 4;
+  public static final int COL_MICE_DESTINATION = 5;
+  public static final int COL_MICE_HOTELCATEGORY = 6;
+  public static final int COL_MICE_TOTALPAX = 7;
+  public static final int COL_MICE_BUDGET = 8;
+  public static final int COL_MICE_CHECKIN = 9;
+  public static final int COL_MICE_CHECKOUT = 10;
+
+  // insurance
+
+  public static final int COL_INSURANCE_JOURNEYTYPE = 4;
+  public static final int COL_INSURANCE_SOURCE = 5;
+  public static final int COL_INSURANCE_DESTINATION = 6;
+  public static final int COL_INSURANCE_PASSENGERS = 7;
+  public static final int COL_INSURANCE_ONWARDDATE = 8;
+  public static final int COL_INSURANCE_RETURNDATE = 9;
+
+}
